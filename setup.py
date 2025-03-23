91ec3af546bcf324d3f62c6cdd03014f96b5f6c4
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2025-03-23 14:18:18 -0700

    seetup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="hyrl",
+    name="HyRL",
     version="1.0.0",
     description="Hybrid Reinforcement Learning for Obstacle Avoidance",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="HSL-UCSC",
     url="https://github.com/HSL-UCSC/ObstacleAvoidanceHyRL",
-    # If modules are in the repository root, list them here:
-    py_modules=["HyRL", "obstacleavoidance_env", "utils"],
+    # Automatically find packages, but exclude 'examples' and 'tests' directories.
+    packages=find_packages(exclude=["examples", "tests"]),
+    # package_data={
+    #     # This tells setuptools to include all files under the "models" subdirectory
+    #     # of the HyRL package.
+    #     "HyRL": ["models/*"],
+    # },
     install_requires=[
         "gym",
         "torch",
@@ -17,5 +22,8 @@ setup(
         "numpy",
         "stable-baselines3",
     ],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
